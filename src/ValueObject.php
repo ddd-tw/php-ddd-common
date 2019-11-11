d5063aa1ed48d54b902e2213c4f66a1ed9d831d3
Eric G. Huang <eric.g.yuan@gmail.com>
2019-11-11 10:50:20 +0800

    Add ValueObject description and test

--- src/ValueObject.php
+++ src/ValueObject.php
@@ -6,6 +6,18 @@ namespace DDDTW\DDD\Common;
 
 abstract class ValueObject
 {
+    /**
+     * Flat your value object implementation into an array, then you can use equals method to compare two value objects
+     *  Example:
+     *     // abstract public function getEqualityComponents(): array { return [$this->name, $this->amount];}
+     *
+     *  $orderItem1 = new OrderItem('item_name', 100);
+     *  $orderItem2 = new OrderItem('item_name', 100);
+     *
+     *  $orderItem1->equals($orderItem2); // return true
+     *
+     * @return array
+     */
     abstract public function getEqualityComponents(): array;
 
     public function equals(?ValueObject $vo): bool
--- tests/ValueObjectTest.php
+++ tests/ValueObjectTest.php
@@ -23,11 +23,23 @@ class ValueObjectTest extends TestCase
 
         $this->assertFalse($personA->equals($personB));
     }
+
+    public function testEquals_PropertyIsValueObjectAndIsTheSame_ReturnTrue(): void
+    {
+        $person1 = new TestPerson('Eric', 34);
+        $person2 = new TestPerson('Joanne', 18);
+
+        $group1 = new TestGroup([$person1, $person2]);
+        $group2 = new TestGroup([$person1, $person2]);
+
+        $this->assertTrue($group1->equals($group2));
+    }
 }
 
 class TestPerson extends ValueObject
 {
     private $name;
+
     private $age;
 
     public function __construct(string $name, int $age)
@@ -36,11 +48,64 @@ class TestPerson extends ValueObject
         $this->age = $age;
     }
 
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAge(): int
+    {
+        return $this->age;
+    }
+
     public function getEqualityComponents(): array
     {
         return [
             $this->name,
             $this->age,
         ];
+    }
+}
+
+class TestGroup extends ValueObject
+{
+    /**
+     * @var TestPerson[]
+     */
+    private $persons;
+
+    public function __construct(array $person)
+    {
+        $this->persons = $person;
+    }
+
+    /**
+     * Flat your value object implementation into an array, then you can use equals method to compare two value objects
+     *  Example:
+     *     // abstract public function getEqualityComponents(): array { return [$this->name, $this->amount];}
+     *
+     *  $orderItem1 = new OrderItem('item_name', 100);
+     *  $orderItem2 = new OrderItem('item_name', 100);
+     *
+     *  $orderItem1->equals($orderItem2); // return true
+     *
+     * @return array
+     */
+    public function getEqualityComponents(): array
+    {
+        $components = [];
+
+        foreach ($this->persons as $person) {
+            $components[] = $person->getName();
+            $components[] = $person->getAge();
+        }
+
+        return $components;
     }
 }
