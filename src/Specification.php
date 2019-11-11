b71b9ea154824cb26317ad2300b88c11831b185d
Eric G. Huang <eric.g.yuan@gmail.com>
2019-11-11 11:12:17 +0800

    Add Specification description

--- src/Specification.php
+++ src/Specification.php
@@ -8,6 +8,17 @@ abstract class Specification
 {
     protected $entity;
 
+    /**
+     * return a function including the condition for verifying properties
+     * Example:
+     *  abstract public function predicate(): callable {
+            return function() {
+     *          return !empty($this->someProp);
+     *      }
+     *  }
+     *
+     * @return callable
+     */
     abstract public function predicate(): callable;
 
     public function isSatisfy(): bool {
