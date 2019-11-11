e2cb7de0bc541d6e8a1f3223bf46ec756ed7ea87
Eric G. Huang <eric.g.yuan@gmail.com>
2019-11-11 09:57:37 +0800

    Add Entity abstract class description

--- src/Entity.php
+++ src/Entity.php
@@ -6,6 +6,18 @@ namespace DDDTW\DDD\Common;
 
 abstract class Entity
 {
+    /**
+     * Returning a property as a entity identity in the implementing class because every entity must define an identity
+     * Example:
+     *  return $this->orderId;
+     * You could use equals method to compare two entities after you implement this method
+     * Example:
+     *  $order = new Order();
+     *  $anotherOrder = new Order();
+     *  $order->equals($order);
+     *
+     * @return string
+     */
     abstract public function getIdentity(): string;
 
     private $eventSuppressed = false;
